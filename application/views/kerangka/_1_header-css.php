4b8d7c811048dc2d69737b9c55a20359af1dc992
Hambaallah14 <fahmiazri11@gmail.com>
2022-03-10 01:04:56 +0700

    Mengedit dan Mengubah Data

---
+++ application/views/kerangka/_1_header-css.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Mazer Admin Dashboard</title>
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    
+    <link rel="stylesheet" href="assets/vendors/iconly/bold.css">
+
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+
+    <!-- Datatables -->
+    <link rel="stylesheet" href="assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/vendors/fontawesome/all.min.css">
+    <style>
+        table.dataTable td{
+            padding: 15px 8px;
+        }
+        .fontawesome-icons .the-icon svg {
+            font-size: 24px;
+        }
+    </style>
+    <!-- END Datatables -->
+
+    <!-- SWEET ALERTS -->
+    <link rel="stylesheet" href="assets/vendors/sweetalert2/sweetalert2.min.css">
+    <!-- END SWEET ALERTS -->
+
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="assets/vendors/fontawesome/all.min.css">
+    <style>
+        .fontawesome-icons {
+            text-align: center;
+        }
+
+        article dl {
+            background-color: rgba(0, 0, 0, .02);
+            padding: 20px;
+        }
+
+        .fontawesome-icons .the-icon svg {
+            font-size: 24px;
+        }
+    </style>
+    <!-- END FONTAWESOME -->
+
+    <!-- DRIPICONS -->
+    <link rel="stylesheet" href="assets/vendors/dripicons/webfont.css">
+    <link rel="stylesheet" href="assets/css/pages/dripicons.css">
+    <!-- END DRIPICONS -->
+
+    <!-- CHART -->
+    <link rel="stylesheet" href="assets/vendors/chartjs/Chart.min.css">
+    <!-- END CHART -->
+</head>
+
+<body>
+    <div id="app">
