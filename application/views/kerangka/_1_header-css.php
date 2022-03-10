ae26ae74116ea1f4478444819ec38caadb30e132
Hambaallah14 <fahmiazri11@gmail.com>
2022-03-10 07:04:35 +0700

    Mengubah link address untuk CSS dan JS

--- application/views/kerangka/_1_header-css.php
+++ application/views/kerangka/_1_header-css.php
@@ -8,18 +8,18 @@
     
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap.css">
     
-    <link rel="stylesheet" href="assets/vendors/iconly/bold.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/iconly/bold.css">
 
-    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
-    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/app.css">
-    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/css/app.css">
+    <link rel="shortcut icon" href="<?=base_url();?>assets/images/favicon.svg" type="image/x-icon">
 
     <!-- Datatables -->
-    <link rel="stylesheet" href="assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="assets/vendors/fontawesome/all.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/jquery-datatables/jquery.dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/fontawesome/all.min.css">
     <style>
         table.dataTable td{
             padding: 15px 8px;
@@ -31,11 +31,11 @@
     <!-- END Datatables -->
 
     <!-- SWEET ALERTS -->
-    <link rel="stylesheet" href="assets/vendors/sweetalert2/sweetalert2.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/sweetalert2/sweetalert2.min.css">
     <!-- END SWEET ALERTS -->
 
     <!-- FONTAWESOME -->
-    <link rel="stylesheet" href="assets/vendors/fontawesome/all.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/fontawesome/all.min.css">
     <style>
         .fontawesome-icons {
             text-align: center;
@@ -53,12 +53,12 @@
     <!-- END FONTAWESOME -->
 
     <!-- DRIPICONS -->
-    <link rel="stylesheet" href="assets/vendors/dripicons/webfont.css">
-    <link rel="stylesheet" href="assets/css/pages/dripicons.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/dripicons/webfont.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/css/pages/dripicons.css">
     <!-- END DRIPICONS -->
 
     <!-- CHART -->
-    <link rel="stylesheet" href="assets/vendors/chartjs/Chart.min.css">
+    <link rel="stylesheet" href="<?=base_url();?>assets/vendors/chartjs/Chart.min.css">
     <!-- END CHART -->
 </head>
 
--- application/views/kerangka/_4_footer-js.php
+++ application/views/kerangka/_4_footer-js.php
@@ -1,39 +1,39 @@
         
         </div> <!-- END APP -->
 
-        <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-        <script src="assets/js/bootstrap.bundle.min.js"></script>    
-        <script src="assets/vendors/apexcharts/apexcharts.js"></script>
-        <script src="assets/js/pages/dashboard.js"></script>
-        <script src="assets/js/mazer.js"></script>
+        <script src="<?=base_url();?>assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+        <script src="<?=base_url();?>assets/js/bootstrap.bundle.min.js"></script>    
+        <script src="<?=base_url();?>assets/vendors/apexcharts/apexcharts.js"></script>
+        <script src="<?=base_url();?>assets/js/pages/dashboard.js"></script>
+        <script src="<?=base_url();?>assets/js/mazer.js"></script>
 
         <!-- DATATABLES -->
-        <script src="assets/vendors/jquery/jquery.min.js"></script>
-        <script src="assets/vendors/jquery-datatables/jquery.dataTables.min.js"></script>
-        <script src="assets/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js"></script>
-        <script src="assets/vendors/fontawesome/all.min.js"></script>
+        <script src="<?=base_url();?>assets/vendors/jquery/jquery.min.js"></script>
+        <script src="<?=base_url();?>assets/vendors/jquery-datatables/jquery.dataTables.min.js"></script>
+        <script src="<?=base_url();?>assets/vendors/jquery-datatables/custom.jquery.dataTables.bootstrap5.min.js"></script>
+        <script src="<?=base_url();?>assets/vendors/fontawesome/all.min.js"></script>
         <script>
             let jquery_datatable = $("#table1").DataTable()
         </script>
         <!-- END DATATABLES -->
 
         <!-- SWEET ALERTS -->
-        <script src="assets/js/extensions/sweetalert2.js"></script>
-        <script src="assets/vendors/sweetalert2/sweetalert2.all.min.js"></script>
+        <script src="<?=base_url();?>assets/js/extensions/sweetalert2.js"></script>
+        <script src="<?=base_url();?>assets/vendors/sweetalert2/sweetalert2.all.min.js"></script>
         <!-- END SWEET ALERTS -->
 
         <!-- FONTAWESOME -->
-        <script src="assets/vendors/fontawesome/all.min.js"></script>
+        <script src="<?=base_url();?>assets/vendors/fontawesome/all.min.js"></script>
         <!-- END FONTAWESOME -->
 
         <!-- CHART -->
-        <script src="assets/vendors/chartjs/Chart.min.js"></script>
-        <script src="assets/js/pages/ui-chartjs.js"></script>
+        <script src="<?=base_url();?>assets/vendors/chartjs/Chart.min.js"></script>
+        <script src="<?=base_url();?>assets/js/pages/ui-chartjs.js"></script>
         <!-- END CHART -->
 
         <!-- SELECT JS -->
-        <script src="assets/vendors/choices.js/choices.min.js"></script>
-        <script src="assets/js/pages/form-element-select.js"></script>
+        <script src="<?=base_url();?>assets/vendors/choices.js/choices.min.js"></script>
+        <script src="<?=base_url();?>assets/js/pages/form-element-select.js"></script>
         <!-- END SELECT JS -->
     </body>
 </html>
